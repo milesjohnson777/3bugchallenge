4a51da36a35372865bf136ef7e3ea9f82d9e72b5
Miles Johnson <milesjohnsonwashere@gmail.com>
2016-02-24 23:21:52 -0600

    first commit, used numered inline comments to explain my line of thinking in the order i thought them

---
+++ solo_script.js
@@ -0,0 +1,84 @@
+// ! ! !
+// Three Bugs
+
+var arrayAtticus = ["Atticus", "2405", "47000", 3];
+var arrayJem = ["Jem", "62347", "63500", 4];
+var arrayBoo = ["Boo", "11435", "54000", 3];
+var arrayScout = ["Scout", "6243", "74750", 5];
+
+var array = [arrayAtticus, arrayJem, arrayBoo, arrayScout];
+
+//Create variables used to write to the DOM
+var newEl, newText, position;
+//Capture the position of insertion into the DOM
+position = document.getElementById('content');
+
+//Loop the array, extracting each array and writing information to the DOM
+//Note that the information is not 'clean'
+for(var i = 0; i < array.length; i++){
+	array[i] = calculateSTI(array[i]);  //2. must be here b/c calculatesSTI is gets passed in an argument of 'array' insted of 'array[i]'? tested = progress
+ 	newEl = document.createElement('li');
+	newText = document.createTextNode(array[i]); //1. possibly here b/c array[1] should be newArray maybe?  tested = nope.
+	newEl.appendChild(newText);
+	position.appendChild(newEl);
+}
+
+function calculateSTI(array){
+  var newArray = [];
+
+  newArray[0] = array[0];
+
+  var employeeNumber = array[1];
+  var baseSalary = array[2];
+  var reviewScore = array[3];
+                             //4. line 35 needs to be rounded to the nearest dollar. tested = WAY OFF 
+  var bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
+  if(bonus > 0.13){
+    bonus = 0.13;
+  }
+
+  newArray[1] = bonus;
+  newArray[2] = Math.round(baseSalary * (1.0 + bonus)); //6. ah ha! i think... lets find out. tested = YES! I win the game. BTW Ryan you just lost the game (assuming you know what that is)
+  newArray[3] = Math.round(baseSalary * bonus);  //5. ok, this has to be where the Math.round() needs to be. tested = nope but leaving it in b/c thats what the initial instructions asked for.
+  console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
+  return newArray;
+}
+
+function getBaseSTI(reviewScore){
+  var basePercent;
+  switch(reviewScore){
+    case 1:
+      basePercent = 0;
+      break;
+    case 2:
+      basePercent = 0;
+      break;
+    case 3:
+      basePercent = 0.04;
+      break;
+    case 4:
+      basePercent = 0.06;
+      break;
+    case 5:
+      basePercent = 0.10;
+      break;
+  }
+  return basePercent; //3. this is where we got nagative numbers b/c all switch case are less than 1 //tested = progress!
+}
+
+function getYearAdjustment(employeeNumber){
+  var yearAdjustment = 0;
+  if(employeeNumber.length == 4){
+    yearAdjustment = 0.05;
+  }
+  return yearAdjustment;
+}
+
+function getIncomeAdjustment(salary){
+  var incomeAdjustment = 0;
+  salary = parseInt(salary);
+  if(salary > 65000){
+    incomeAdjustment = 0.01; 
+  }
+  return incomeAdjustment;
+}
